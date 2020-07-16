03958a643ed59eb732c4990899654a4392553347
Zac <a19860101@gmail.com>
2020-07-16 19:40:00 +0800

    20200716

--- file/upload.php
+++ file/upload.php
@@ -5,7 +5,20 @@
    $size = $_FILES["img"]["size"];
    $type = $_FILES["img"]["type"];
    $error = $_FILES["img"]["error"];
-    $target = "images/{$name}";
+    // $target = "images/{$name}";
+    switch($type){
+        case "image/jpeg":
+            $target = "images/".md5(time()).".jpg";
+        break;
+        case "image/png":
+            $target = "images/".md5(time()).".png";
+        break;
+        case "image/gif":
+            $target = "images/".md5(time()).".gif";
+        break;
+    }
+
+    
 
    if($error == 0){
        if(move_uploaded_file($tmp_name,$target)){
