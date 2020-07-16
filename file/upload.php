19806de0301b53fe0a88a867c70ba705caaa6f6d
Zac <a19860101@gmail.com>
2020-07-16 19:18:15 +0800

    20200716

--- file/upload.php
+++ file/upload.php
@@ -1,1 +1,19 @@
- 
+<?php
+//    var_dump( $_FILES["img"] );
+   $name = $_FILES["img"]["name"];
+   $tmp_name = $_FILES["img"]["tmp_name"];
+   $size = $_FILES["img"]["size"];
+   $type = $_FILES["img"]["type"];
+   $error = $_FILES["img"]["error"];
+   
+    $target = "images/{$name}";
+
+   if($error == 0){
+       if(move_uploaded_file($tmp_name,$target)){
+            echo  "上傳成功";
+       }else{
+           echo "上傳失敗";
+       }
+   }else{
+       echo "上傳錯誤";
+   }
