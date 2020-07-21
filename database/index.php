fb2a290bd9460326a74ccbe340f8892581b98b1e
Zac <a19860101@gmail.com>
2020-07-21 20:29:58 +0800

    20200721

--- database/index.php
+++ database/index.php
@@ -10,19 +10,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
 <body>
-    <table>
-    <?php while($row=mysqli_fetch_assoc($result)){ ?>
-        <tr>
-            <td><?php echo $row["id"];?></td>
-            <td><?php echo $row["name"];?></td>
-            <td><?php echo $row["mail"];?></td>
-            <td><?php echo $row["phone"];?></td>
-            <td><?php echo $row["gender"];?></td>
-            <td><?php echo $row["create_at"];?></td>
-        </tr>
-    <?php } ?>
-    </table>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <table class="table">
+                    <tr>
+                        <th>#</th>
+                        <th>姓名</th>
+                        <th>Mail</th>
+                        <th>電話</th>
+                        <th>性別</th>
+                        <th>建立時間</th>
+                    </tr>
+                    <?php while($row=mysqli_fetch_assoc($result)){ ?>
+                    <tr>
+                        <td><?php echo $row["id"];?></td>
+                        <td><?php echo $row["name"];?></td>
+                        <td><?php echo $row["mail"];?></td>
+                        <td><?php echo $row["phone"];?></td>
+                        <td><?php echo $row["gender"];?></td>
+                        <td><?php echo $row["create_at"];?></td>
+                    </tr>
+                    <?php } ?>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
 </html>
