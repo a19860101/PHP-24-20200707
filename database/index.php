bcfd1212c2eae7627be8712941f9d1e64c2e9a8f
Zac <a19860101@gmail.com>
2020-07-21 19:24:21 +0800

    20200721

---
+++ database/index.php
@@ -0,0 +1,19 @@
+<?php
+    $db_host = "localhost";
+    $db_user = "admin";
+    $db_pw = "admin";
+    $db_name = "php0707";
+
+    $conn = mysqli_connect($db_host,$db_user,$db_pw,$db_name);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
