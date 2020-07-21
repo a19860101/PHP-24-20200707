035417215ab43c4d81c21d4fd0d96637e387e961
Zac <a19860101@gmail.com>
2020-07-21 20:11:32 +0800

    20200721

--- database/index.php
+++ database/index.php
@@ -4,14 +4,15 @@
     $sql = "SELECT * FROM students";
     $result = mysqli_query($conn,$sql);
 
-    $row = mysqli_fetch_assoc($result);
-
-    echo $row["id"];
-    echo $row["name"];
-    echo $row["phone"];
-    echo $row["mail"];
-    echo $row["gender"];
-    echo $row["create_at"];
+    while($row = mysqli_fetch_assoc($result)){
+        echo $row["id"];
+        echo $row["name"];
+        echo $row["phone"];
+        echo $row["mail"];
+        echo $row["gender"];
+        echo $row["create_at"];
+        echo "<br>";
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
