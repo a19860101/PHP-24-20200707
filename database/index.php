a9905e819ec7e9eceaad4a270c6975b12a13b0f9
Zac <a19860101@gmail.com>
2020-07-21 19:35:26 +0800

    20200721

--- database/index.php
+++ database/index.php
@@ -4,7 +4,11 @@
     $db_pw = "admin";
     $db_name = "php0707";
 
-    $conn = mysqli_connect($db_host,$db_user,$db_pw,$db_name);
+    $conn = mysqli_connect($db_host,$db_user,$db_pw,$db_name)or die("資料庫連結失敗，請洽網站管理員");
+
+    mysqli_query($conn,"SET NAMES utf8");
+
+    
 ?>
 <!DOCTYPE html>
 <html lang="en">
