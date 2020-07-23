bbacb4b7c2aea800084656296784a513760bed05
Zac <a19860101@gmail.com>
2020-07-23 20:58:37 +0800

    20200723

--- pdo/store.php
+++ pdo/store.php
@@ -1,20 +1,4 @@
 <?php
-//     try {
-//         require_once("pdo.php"); 
-//         $name = $_POST["name"];
-//         $mail = $_POST["mail"];
-//         $phone = $_POST["phone"];
-//         $gender = $_POST["gender"];
-
-//         $sql = "INSERT INTO students(name,mail,phone,gender,create_at)VALUES(?,?,?,?,?)";
-//         $stmt = $pdo->prepare($sql);
-//         $stmt->execute([$name,$mail,$phone,$gender,$now]);
-
-//         header("location:index.php");
-
-//     }catch(PDOException $e){
-//         echo $e->getMessage();
-//     }
     try {
         require_once("pdo.php"); 
         $name = $_POST["name"];
@@ -22,18 +6,34 @@
         $phone = $_POST["phone"];
         $gender = $_POST["gender"];
 
-        $sql = "INSERT INTO students(name,mail,phone,gender,create_at)VALUES(:name,:mail,:phone,:gender,:create_at)";
+        $sql = "INSERT INTO students(name,mail,phone,gender,create_at)VALUES(?,?,?,?,?)";
         $stmt = $pdo->prepare($sql);
-        $stmt->bindParam(":name",$name);
-        $stmt->bindParam(":mail",$mail);
-        $stmt->bindParam(":phone",$phone);
-        $stmt->bindParam(":gender",$gender);
-        $stmt->bindParam(":create_at",$now);
-
-        $stmt->execute();
+        $stmt->execute([$name,$mail,$phone,$gender,$now]);
 
         header("location:index.php");
 
     }catch(PDOException $e){
         echo $e->getMessage();
     }
+//     try {
+//         require_once("pdo.php"); 
+//         $name = $_POST["name"];
+//         $mail = $_POST["mail"];
+//         $phone = $_POST["phone"];
+//         $gender = $_POST["gender"];
+
+//         $sql = "INSERT INTO students(name,mail,phone,gender,create_at)VALUES(:name,:mail,:phone,:gender,:create_at)";
+//         $stmt = $pdo->prepare($sql);
+//         $stmt->bindParam(":name",$name);
+//         $stmt->bindParam(":mail",$mail);
+//         $stmt->bindParam(":phone",$phone);
+//         $stmt->bindParam(":gender",$gender);
+//         $stmt->bindParam(":create_at",$now);
+
+//         $stmt->execute();
+
+//         header("location:index.php");
+
+//     }catch(PDOException $e){
+//         echo $e->getMessage();
+//     }
