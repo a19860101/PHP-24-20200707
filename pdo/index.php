8e070b4b322017678ef4445a2547fb054ea2f914
Zac <a19860101@gmail.com>
2020-07-23 20:08:47 +0800

    20200723

---
+++ pdo/index.php
@@ -0,0 +1,53 @@
+<?php
+    try {
+        require_once("pdo.php");
+        $sql = "SELECT * FROM students";
+        //預備陳述式 prepare statement
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+        $rows = array();
+        while($row = $stmt -> fetch()){
+            $rows[] = $row;
+        }
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+?>
+<?php include("template/header.php");?>
+<?php include("template/nav.php");?>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <table class="table">
+                    <tr>
+                        <th>#</th>
+                        <th>姓名</th>
+                        <th>Mail</th>
+                        <th>電話</th>
+                        <th>性別</th>
+                        <th>建立時間</th>
+                        <th>動作</th>
+                    </tr>
+                    <?php foreach($rows as $row){ ?>
+                        <tr>
+                        <td><?php echo $row["id"];?></td>
+                        <td><?php echo $row["name"];?></td>
+                        <td><?php echo $row["mail"];?></td>
+                        <td><?php echo $row["phone"];?></td>
+                        <td><?php echo $row["gender"];?></td>
+                        <td><?php echo $row["create_at"];?></td>
+                        <td>
+                            <form action="delete.php" method="post">
+                                <input type="hidden" name="id" value="<?php echo $row["id"];?>">
+                                <input type="submit" value="刪除" class="btn btn-danger btn-sm" onclick="return confirm('確認刪除？')">
+                            </form>
+                            <a href="edit.php?id=<?php echo $row["id"];?>" class="btn btn-success btn-sm">編輯</a>
+                        </td>
+                    </tr>
+                    <?php }?>
+                </table>
+            </div>
+        </div>
+    </div>
+
+<?php include("template/footer.php");?>
--- pdo/pdo.php
+++ pdo/pdo.php
@@ -1,11 +1,17 @@
 <?php
-   $db_host = "localhost";
-   $db_user = "admin";
-   $db_pw = "admin";
-   $db_name = "php0707";
-   $db_charset = "utf8mb4";
+    $db_host = "localhost";
+    $db_user = "admin";
+    $db_pw = "admin";
+    $db_name = "php0707";
+    $db_charset = "utf8mb4";
 
-   $dsn = "mysql:host={$db_host};dbname={$db_name};charset={$db_charset}";
-//    $dsn = "mysql:host=localhost;dbname=php0707;charset=utf8mb4";
-
-   $pdo = new PDO($dsn,$db_user,$db_pw);
+    date_default_timezone_set("Asia/Taipei");
+    $now = date("Y-m-d H:i:s");
+    
+    try {
+        $dsn = "mysql:host={$db_host};dbname={$db_name};charset={$db_charset}";
+        // $dsn = "mysql:host=localhost;dbname=php0707;charset=utf8mb4";
+        $pdo = new PDO($dsn,$db_user,$db_pw);
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
