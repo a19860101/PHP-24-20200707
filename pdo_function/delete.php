d234a7be8f65ab8fb124cca3857a0b38fba72ec5
Zac <a19860101@gmail.com>
2020-07-28 19:06:02 +0800

    20200728

--- pdo_function/delete.php
+++ pdo_function/delete.php
@@ -1,22 +1,5 @@
 <?php
-    try{
-        require_once("pdo.php");
-        $id = $_POST["id"];
-        $sql = "DELETE FROM students WHERE id = ?";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([$id]);
-        header("location:index.php");
-    }catch(PDOException $e){
-        echo $e->getMessage();
-    }
-    // try{
-    //     require_once("pdo.php");
-    //     $id = $_POST["id"];
-    //     $sql = "DELETE FROM students WHERE id = :id";
-    //     $stmt = $pdo->prepare($sql);
-    //     $stmt->bindParam(":id",$id);
-    //     $stmt->execute();
-    //     header("location:index.php");
-    // }catch(PDOException $e){
-    //     echo $e->getMessage();
-    // }
+    include("function.php");
+    $id = $_POST["id"];
+    delete($id);
+    header("location:index.php");
