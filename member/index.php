8c88ab99d152c8f93ca1b2d335a6593f355bc9d0
Zac <a19860101@gmail.com>
2020-07-28 21:57:00 +0800

    20200728

--- member/index.php
+++ member/index.php
@@ -1,3 +1,14 @@
 <?php include("template/header.php");?>
 <?php include("template/nav.php");?>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <?php if($_SESSION){ ?>
+            <div><?php echo $_SESSION["USER"];?>你好</div>
+            <?php }else{ ?>
+            <div>訪客你好</div>
+            <?php }?>
+        </div>
+    </div>
+</div>
 <?php include("template/footer.php");?>
