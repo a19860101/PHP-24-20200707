1601288a19118d5ccd98588663f4f6725da4d7e7
Zac <a19860101@gmail.com>
2020-07-30 20:07:23 +0800

    20200730

--- photo/upload.php
+++ photo/upload.php
@@ -1,7 +1,13 @@
 <?php
     include("function.php");
     $file = $_FILES["img"];
-    $path = uploadImg($file);
+    
+    if($file["name"] == ""){
+        $path = "no-pic.jpg";
+    }else{
+        $path = uploadImg($file);
+    }
 
     store($path);
+    echo "上傳成功";
     header("refresh:2;url=index.php");
