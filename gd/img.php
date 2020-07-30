a2746c345b5e457e5dfa4c6947f5b63b757e1bae
Zac <a19860101@gmail.com>
2020-07-30 21:28:20 +0800

    20200730

--- gd/img.php
+++ gd/img.php
@@ -1,6 +1,8 @@
 <?php
     $img = "img.jpg";
     $canvas = imagecreatefromjpeg($img);
+    // $canvas = imagecreatefrompng($img);
+    // $canvas = imagecreatefromgif($img);
     $canvas_w = imagesx($canvas);
     $canvas_h = imagesy($canvas);
 
@@ -15,3 +17,5 @@
     imagecopyresampled($new_canvas,$canvas,0,0,0,0,$new_w,$new_h,$canvas_w,$canvas_h);
 
     imagejpeg($new_canvas,"test.jpg");
+    // imagepng($new_canvas,"test.png");
+    // imagegif($new_canvas,"test.gif");
--- photo/function.php
+++ photo/function.php
@@ -13,13 +13,13 @@
         $type = $file["type"];
         switch($type){
             case "image/jpeg":
-                $target = md5(time()).".jpg";
+                $name = md5(time()).".jpg";
             break;
             case "image/png":
-                $target = md5(time()).".png";
+                $name = md5(time()).".png";
             break;
             case "image/gif":
-                $target = md5(time()).".gif";
+                $name = md5(time()).".gif";
             break;
             default:
                 return 'error';
@@ -27,11 +27,36 @@
         $tmp_name = $file["tmp_name"];
         $error =$file["error"];
         $size = $file["size"];
+        $target = "images/".$name;
         if($error == 0){
-            if(move_uploaded_file($tmp_name,"images/".$target));
-            return $target;
+            if(move_uploaded_file($tmp_name,$target)){
+                imgResize($type,$target,$name);
+                return $name;
+            }
         }else{
             return "上傳失敗";
         }
 
+    }
+    function imgResize($type,$target,$name){
+        switch($type){
+            case "image/jpeg":
+                $canvas = imagecreatefromjpeg($target);
+            break;
+            case "image/png":
+                $canvas = imagecreatefrompng($target);
+            break;
+            case "image/gif":
+                $canvas = imagecreatefromgif($target);
+            break;
+        }
+        $canvas_w = imagesx($canvas);
+        $canvas_h = imagesy($canvas);
+        $new_w = 800;
+        $new_h = $canvas_h / $canvas_w * $new_w;
+        $new_canvas = imagecreatetruecolor($new_w,$new_h);
+        imagecopyresampled($new_canvas,$canvas,0,0,0,0,$new_w,$new_h,$canvas_w,$canvas_h);
+        imagejpeg($new_canvas,"thumbs/".$name);
+        imagepng($new_canvas,"thumbs/".$name);
+        imagegif($new_canvas,"thumbs/".$name);
     }
