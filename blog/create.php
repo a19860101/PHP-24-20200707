95bece58ba37819108ed88510cb80862f62e1875
Zac <a19860101@gmail.com>
2020-08-04 20:25:32 +0800

    20200804

--- blog/create.php
+++ blog/create.php
@@ -1,3 +1,7 @@
+<?php 
+    include("function/cate.php");
+    $cates = showAllCates();
+?>
 <?php include("template/header.php");?>
 <?php include("template/nav.php");?>
 <div class="container py-5">
@@ -11,6 +15,14 @@
                 <div class="form-group">
                     <label for="content">內文</label>
                     <textarea name="content" id="content" rows="10" class="form-control"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="c_id">分類</label>
+                    <select name="c_id" id="c_id" class="form-control">
+                        <?php foreach($cates as $cate){ ?>
+                        <option value="<?php echo $cate["id"];?>"><?php echo $cate["title"];?></option>
+                        <?php } ?>
+                    </select>
                 </div>
                 <input type="submit" value="新增文章" class="btn btn-primary">
             </form>
