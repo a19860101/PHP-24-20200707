d4a629733e140f5fd5fb449c3052a535e6f0d862
Zac <a19860101@gmail.com>
2020-08-04 20:56:07 +0800

    20200804

--- blog/store.php
+++ blog/store.php
@@ -2,7 +2,7 @@
     include("function/post.php");
     $title = $_POST["title"];
     $content = $_POST["content"];
-    $c_id = 1;
+    $c_id = $_POST["c_id"];
     $u_id = 1;
 
     storePost($title,$content,$c_id,$u_id);
