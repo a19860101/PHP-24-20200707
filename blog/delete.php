c2015106c4da2dc1b6bf03ae2d16ac0693d702c7
Zac <a19860101@gmail.com>
2020-08-06 19:49:37 +0800

    20200806

--- blog/delete.php
+++ blog/delete.php
@@ -1,4 +1,9 @@
 <?php
     include("function/post.php");
+    if($_POST["cover"] != "no-pic.png"){
+        unlink("images/".$_POST["cover"]);
+        unlink("thumbs/".$_POST["cover"]);
+    }
     deletePost($_POST["id"]);
+
     header("location:index.php");
