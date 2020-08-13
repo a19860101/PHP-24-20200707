2b2253d731bad15218dca9d6df4fc31326b9889b
Zac <a19860101@gmail.com>
2020-08-13 21:51:10 +0800

    20200813

--- oop/index.php
+++ oop/index.php
@@ -7,6 +7,7 @@
         public $age = 18;
         public $gender;
         private $level = 0;
+        protected $test = 999;
         //方法
         function login(){
             $this->level = 10;
@@ -19,9 +20,17 @@
 
     }
     //建立實體
-    $user1 = new User;
-    $user1->name = 'John';
-    $user1->gender = 'Male';
-    echo $user1->mail;
-    $user1->test();
+    // $user1 = new User;
+    // $user1->name = 'John';
+    // $user1->gender = 'Male';
+    // echo $user1->mail;
+    // $user1->test();
     // echo $user1->level;
+    class Member extends User {
+        function qwer(){
+            echo $this->test;
+        }
+    }
+
+    $member1 = new Member;
+    echo $member1->qwer();
